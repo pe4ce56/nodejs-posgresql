a7bf3b89568710e155828924f5411aa19a08f340
Damai Deo Saputro <51025335+pe4ce56@users.noreply.github.com>
2021-02-22 23:00:38 +0700

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/quotes', quotesRouter);
-app.get('/test',(res,req)=>{
+app.use('/test',(res,req)=>{
   res.send("test")
 })
 module.exports = app;
